49060ec69ea72f2c4d2ef42d16f837571e93c4d9
Yoshitaka Totsuka <y047aka@gmail.com>
2021-01-07 02:04:06 +0900

    Add sectionForSite

--- src/Main.elm
+++ src/Main.elm
@@ -3,10 +3,10 @@ module Main exposing (Model, Msg(..), init, main, update, view)
 import Browser
 import Css exposing (..)
 import Css.Global exposing (global)
-import Css.ResetAndCustomize exposing (additionalReset, globalCustomize)
 import Css.Reset exposing (normalize)
+import Css.ResetAndCustomize exposing (additionalReset, globalCustomize)
 import Html
-import Html.Styled exposing (Html, div, main_, p, span, text, toUnstyled)
+import Html.Styled exposing (Html, div, h1, h2, h3, h4, h5, main_, p, span, text, toUnstyled)
 import Html.Styled.Attributes as Attributes exposing (class, css, href, id, rel, target)
 import Html.Styled.Events exposing (onClick)
 import UI exposing (..)
@@ -78,7 +78,8 @@ view model =
             , main_ []
                 [ toc [] [ tableOfContents ]
                 , article []
-                    [ sectionForButtons model
+                    [ sectionForSite
+                    , sectionForButtons model
                     , sectionForHeaders
                     , sectionForLabels
                     , sectionForTexts
@@ -92,14 +93,50 @@ view model =
 
 tableOfContents : Html msg
 tableOfContents =
-    verticalInvertedMenu []
-        [ verticalInvertedMenuLinkItem [ href "#button" ] [ text "Button" ]
-        , verticalInvertedMenuLinkItem [ href "#header" ] [ text "Header" ]
-        , verticalInvertedMenuLinkItem [ href "#label" ] [ text "Label" ]
-        , verticalInvertedMenuLinkItem [ href "#text" ] [ text "Text" ]
-        , verticalInvertedMenuLinkItem [ href "#grid" ] [ text "Grid" ]
-        , verticalInvertedMenuLinkItem [ href "#menu" ] [ text "Menu" ]
-        , verticalInvertedMenuLinkItem [ href "#table" ] [ text "Table" ]
+    verticalInvertedMenu [] <|
+        List.map (\{ url, label } -> verticalInvertedMenuLinkItem [ href url ] [ text label ])
+            [ { url = "#site", label = "Site" }
+            , { url = "#button", label = "Button" }
+            , { url = "#header", label = "Header" }
+            , { url = "#label", label = "Label" }
+            , { url = "#text", label = "Text" }
+            , { url = "#grid", label = "Grid" }
+            , { url = "#menu", label = "Menu" }
+            , { url = "#table", label = "Table" }
+            ]
+
+
+sectionForSite : Html Msg
+sectionForSite =
+    section [ id "site" ]
+        [ example []
+            [ header [] [ text "Headers" ]
+            , p [] [ text "A site can define styles for headers" ]
+            , h1 [] [ text "First Header" ]
+            , h2 [] [ text "Second Header" ]
+            , h3 [] [ text "Third Header" ]
+            , h4 [] [ text "Fourth Header" ]
+            , h5 [] [ text "Fifth Header" ]
+            ]
+        , example []
+            [ header [] [ text "Page Font" ]
+            , p [] [ text "A site can specify styles for page content." ]
+            , p [] [ text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel tincidunt eros, nec venenatis ipsum. Nulla hendrerit urna ex, id sagittis mi scelerisque vitae. Vestibulum posuere rutrum interdum. Sed ut ullamcorper odio, non pharetra eros. Aenean sed lacus sed enim ornare vestibulum quis a felis. Sed cursus nunc sit amet mauris sodales tempus. Nullam mattis, dolor non posuere commodo, sapien ligula hendrerit orci, non placerat erat felis vel dui. Cras vulputate ligula ut ex tincidunt tincidunt. Maecenas eget gravida lorem. Nunc nec facilisis risus. Mauris congue elit sit amet elit varius mattis. Praesent convallis placerat magna, a bibendum nibh lacinia non." ]
+            , p [] [ text "Fusce mollis sagittis elit ut maximus. Nullam blandit lacus sit amet luctus euismod. Duis luctus leo vel consectetur consequat. Phasellus ex ligula, pellentesque et neque vitae, elementum placerat eros. Proin eleifend odio nec velit lacinia suscipit. Morbi mollis ante nec dapibus gravida. In tincidunt augue eu elit porta, vel condimentum purus posuere. Maecenas tincidunt, erat sed elementum sagittis, tortor erat faucibus tellus, nec molestie mi purus sit amet tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris a tincidunt metus. Fusce congue metus aliquam ex auctor eleifend." ]
+            , p [] [ text "Ut imperdiet dignissim feugiat. Phasellus tristique odio eu justo dapibus, nec rutrum ipsum luctus. Ut posuere nec tortor eu ullamcorper. Etiam pellentesque tincidunt tortor, non sagittis nibh pretium sit amet. Sed neque dolor, blandit eu ornare vel, lacinia porttitor nisi. Vestibulum sit amet diam rhoncus, consectetur enim sit amet, interdum mauris. Praesent feugiat finibus quam, porttitor varius est egestas id." ]
+            ]
+        , example []
+            [ header [] [ text "Text Selection" ]
+            , p [] [ text "A site can specify text selection styles." ]
+            , p [] [ text "Fusce mollis sagittis elit ut maximus. Nullam blandit lacus sit amet luctus euismod. Duis luctus leo vel consectetur consequat. Phasellus ex ligula, pellentesque et neque vitae, elementum placerat eros. Proin eleifend odio nec velit lacinia suscipit. Morbi mollis ante nec dapibus gravida. In tincidunt augue eu elit porta, vel condimentum purus posuere. Maecenas tincidunt, erat sed elementum sagittis, tortor erat faucibus tellus, nec molestie mi purus sit amet tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris a tincidunt metus. Fusce congue metus aliquam ex auctor eleifend." ]
+            ]
+        , example []
+            [ header [] [ text "Spacing" ]
+            , p [] [ text "A site can define default spacing for headers and paragraphs" ]
+            , p [] [ text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel tincidunt eros, nec venenatis ipsum. Nulla hendrerit urna ex, id sagittis mi scelerisque vitae. Vestibulum posuere rutrum interdum. Sed ut ullamcorper odio, non pharetra eros. Aenean sed lacus sed enim ornare vestibulum quis a felis. Sed cursus nunc sit amet mauris sodales tempus. Nullam mattis, dolor non posuere commodo, sapien ligula hendrerit orci, non placerat erat felis vel dui. Cras vulputate ligula ut ex tincidunt tincidunt. Maecenas eget gravida lorem. Nunc nec facilisis risus. Mauris congue elit sit amet elit varius mattis. Praesent convallis placerat magna, a bibendum nibh lacinia non." ]
+            , p [] [ text "Fusce mollis sagittis elit ut maximus. Nullam blandit lacus sit amet luctus euismod. Duis luctus leo vel consectetur consequat. Phasellus ex ligula, pellentesque et neque vitae, elementum placerat eros. Proin eleifend odio nec velit lacinia suscipit. Morbi mollis ante nec dapibus gravida. In tincidunt augue eu elit porta, vel condimentum purus posuere. Maecenas tincidunt, erat sed elementum sagittis, tortor erat faucibus tellus, nec molestie mi purus sit amet tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris a tincidunt metus. Fusce congue metus aliquam ex auctor eleifend." ]
+            , p [] [ text "Ut imperdiet dignissim feugiat. Phasellus tristique odio eu justo dapibus, nec rutrum ipsum luctus. Ut posuere nec tortor eu ullamcorper. Etiam pellentesque tincidunt tortor, non sagittis nibh pretium sit amet. Sed neque dolor, blandit eu ornare vel, lacinia porttitor nisi. Vestibulum sit amet diam rhoncus, consectetur enim sit amet, interdum mauris. Praesent feugiat finibus quam, porttitor varius est egestas id." ]
+            ]
         ]
 
 
