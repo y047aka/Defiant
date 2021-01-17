64346608bc6100e926e46668e087fb73cb4b5b1c
Yoshitaka Totsuka <y047aka@gmail.com>
2021-01-18 00:07:11 +0900

    Add UI.Card

--- src/Main.elm
+++ src/Main.elm
@@ -10,10 +10,11 @@ import Html.Styled exposing (Html, a, div, h1, h2, h3, h4, h5, main_, p, span, s
 import Html.Styled.Attributes as Attributes exposing (class, css, href, id, rel, target)
 import Html.Styled.Events exposing (onClick)
 import UI.Button exposing (..)
+import UI.Card as Card exposing (..)
 import UI.Container exposing (..)
 import UI.Example exposing (..)
 import UI.Grid exposing (..)
-import UI.Header exposing (..)
+import UI.Header as Header exposing (..)
 import UI.Label exposing (..)
 import UI.Menu exposing (..)
 import UI.Modifier exposing (Palette(..))
@@ -85,12 +86,13 @@ view model =
                     , sectionForContainers
                     , sectionForHeaders
                     , sectionForLabels
-                    , sectionForPlaceholder
+                    , sectionForPlaceholders
                     , sectionForSegments
                     , sectionForTexts
-                    , sectionForGrid
+                    , sectionForGrids
                     , sectionForMenus
                     , sectionForTables
+                    , sectionForCards
                     ]
                 ]
             ]
@@ -111,6 +113,7 @@ tableOfContents =
             , { url = "#grid", label = "Grid" }
             , { url = "#menu", label = "Menu" }
             , { url = "#table", label = "Table" }
+            , { url = "#card", label = "Card" }
             ]
 
 
@@ -118,7 +121,7 @@ sectionForSite : Html Msg
 sectionForSite =
     exampleContainer [ id "site" ]
         [ example []
-            [ header [] [ text "Headers" ]
+            [ Header.header [] [ text "Headers" ]
             , p [] [ text "A site can define styles for headers" ]
             , h1 [] [ text "First Header" ]
             , h2 [] [ text "Second Header" ]
@@ -127,19 +130,19 @@ sectionForSite =
             , h5 [] [ text "Fifth Header" ]
             ]
         , example []
-            [ header [] [ text "Page Font" ]
+            [ Header.header [] [ text "Page Font" ]
             , p [] [ text "A site can specify styles for page content." ]
             , p [] [ text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel tincidunt eros, nec venenatis ipsum. Nulla hendrerit urna ex, id sagittis mi scelerisque vitae. Vestibulum posuere rutrum interdum. Sed ut ullamcorper odio, non pharetra eros. Aenean sed lacus sed enim ornare vestibulum quis a felis. Sed cursus nunc sit amet mauris sodales tempus. Nullam mattis, dolor non posuere commodo, sapien ligula hendrerit orci, non placerat erat felis vel dui. Cras vulputate ligula ut ex tincidunt tincidunt. Maecenas eget gravida lorem. Nunc nec facilisis risus. Mauris congue elit sit amet elit varius mattis. Praesent convallis placerat magna, a bibendum nibh lacinia non." ]
             , p [] [ text "Fusce mollis sagittis elit ut maximus. Nullam blandit lacus sit amet luctus euismod. Duis luctus leo vel consectetur consequat. Phasellus ex ligula, pellentesque et neque vitae, elementum placerat eros. Proin eleifend odio nec velit lacinia suscipit. Morbi mollis ante nec dapibus gravida. In tincidunt augue eu elit porta, vel condimentum purus posuere. Maecenas tincidunt, erat sed elementum sagittis, tortor erat faucibus tellus, nec molestie mi purus sit amet tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris a tincidunt metus. Fusce congue metus aliquam ex auctor eleifend." ]
             , p [] [ text "Ut imperdiet dignissim feugiat. Phasellus tristique odio eu justo dapibus, nec rutrum ipsum luctus. Ut posuere nec tortor eu ullamcorper. Etiam pellentesque tincidunt tortor, non sagittis nibh pretium sit amet. Sed neque dolor, blandit eu ornare vel, lacinia porttitor nisi. Vestibulum sit amet diam rhoncus, consectetur enim sit amet, interdum mauris. Praesent feugiat finibus quam, porttitor varius est egestas id." ]
             ]
         , example []
-            [ header [] [ text "Text Selection" ]
+            [ Header.header [] [ text "Text Selection" ]
             , p [] [ text "A site can specify text selection styles." ]
             , p [] [ text "Fusce mollis sagittis elit ut maximus. Nullam blandit lacus sit amet luctus euismod. Duis luctus leo vel consectetur consequat. Phasellus ex ligula, pellentesque et neque vitae, elementum placerat eros. Proin eleifend odio nec velit lacinia suscipit. Morbi mollis ante nec dapibus gravida. In tincidunt augue eu elit porta, vel condimentum purus posuere. Maecenas tincidunt, erat sed elementum sagittis, tortor erat faucibus tellus, nec molestie mi purus sit amet tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris a tincidunt metus. Fusce congue metus aliquam ex auctor eleifend." ]
             ]
         , example []
-            [ header [] [ text "Spacing" ]
+            [ Header.header [] [ text "Spacing" ]
             , p [] [ text "A site can define default spacing for headers and paragraphs" ]
             , p [] [ text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel tincidunt eros, nec venenatis ipsum. Nulla hendrerit urna ex, id sagittis mi scelerisque vitae. Vestibulum posuere rutrum interdum. Sed ut ullamcorper odio, non pharetra eros. Aenean sed lacus sed enim ornare vestibulum quis a felis. Sed cursus nunc sit amet mauris sodales tempus. Nullam mattis, dolor non posuere commodo, sapien ligula hendrerit orci, non placerat erat felis vel dui. Cras vulputate ligula ut ex tincidunt tincidunt. Maecenas eget gravida lorem. Nunc nec facilisis risus. Mauris congue elit sit amet elit varius mattis. Praesent convallis placerat magna, a bibendum nibh lacinia non." ]
             , p [] [ text "Fusce mollis sagittis elit ut maximus. Nullam blandit lacus sit amet luctus euismod. Duis luctus leo vel consectetur consequat. Phasellus ex ligula, pellentesque et neque vitae, elementum placerat eros. Proin eleifend odio nec velit lacinia suscipit. Morbi mollis ante nec dapibus gravida. In tincidunt augue eu elit porta, vel condimentum purus posuere. Maecenas tincidunt, erat sed elementum sagittis, tortor erat faucibus tellus, nec molestie mi purus sit amet tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris a tincidunt metus. Fusce congue metus aliquam ex auctor eleifend." ]
@@ -152,7 +155,7 @@ sectionForButtons : Model -> Html Msg
 sectionForButtons model =
     exampleContainer [ id "button" ]
         [ example []
-            [ header [] [ text "Button" ]
+            [ Header.header [] [ text "Button" ]
             , p [] [ text "A standard button" ]
             , button [] [ text "Follow" ]
             ]
@@ -161,11 +164,11 @@ sectionForButtons model =
             , button [] [ text "Focusable" ]
             ]
         , example []
-            [ header [] [ text "Animated" ]
+            [ Header.header [] [ text "Animated" ]
             , p [] [ text "A button can animate to show hidden content" ]
             ]
         , example []
-            [ header [] [ text "Labeled" ]
+            [ Header.header [] [ text "Labeled" ]
             , p [] [ text "A button can appear alongside a label" ]
             , labeledButton []
                 [ button [] [ text "Like" ]
@@ -178,12 +181,12 @@ sectionForButtons model =
                 ]
             ]
         , example []
-            [ header [] [ text "Basic" ]
+            [ Header.header [] [ text "Basic" ]
             , p [] [ text "A basic button is less pronounced" ]
             , basicButton [] [ text "Add Friend" ]
             ]
         , example [] <|
-            [ header [] [ text "Colored" ]
+            [ Header.header [] [ text "Colored" ]
             , p [] [ text "A button can have different colors" ]
             ]
                 ++ [ primaryButton [] [ text "Primary" ]
@@ -219,12 +222,12 @@ sectionForContainers =
     in
     exampleContainer [ id "container" ]
         [ example []
-            [ header [] [ text "Container" ]
+            [ Header.header [] [ text "Container" ]
             , p [] [ text "A standard container" ]
             , container [] [ content ]
             ]
         , example []
-            [ header [] [ text "Text Container" ]
+            [ Header.header [] [ text "Text Container" ]
             , p [] [ text "A container can reduce its maximum width to more naturally accomodate a single column of text" ]
             , textContainer []
                 [ h2 [] [ text "Header" ]
@@ -239,7 +242,7 @@ sectionForHeaders : Html Msg
 sectionForHeaders =
     exampleContainer [ id "header" ]
         [ example []
-            [ header [] [ text "Content Headers" ]
+            [ Header.header [] [ text "Content Headers" ]
             , p [] [ text "Headers may be oriented to give the importance of a section in the context of the content that surrounds it" ]
             , massiveHeader [] [ text "Massive Header" ]
             , wireframeParagraph
@@ -265,17 +268,17 @@ sectionForLabels : Html Msg
 sectionForLabels =
     exampleContainer [ id "label" ]
         [ example []
-            [ header [] [ text "Label" ]
+            [ Header.header [] [ text "Label" ]
             , p [] [ text "A label" ]
             , label [] [ text "23" ]
             ]
         , example []
-            [ header [] [ text "Basic" ]
+            [ Header.header [] [ text "Basic" ]
             , p [] [ text "A label can reduce its complexity" ]
             , basicLabel [] [ text "Basic" ]
             ]
         , example [] <|
-            [ header [] [ text "Colored" ]
+            [ Header.header [] [ text "Colored" ]
             , p [] [ text "A label can have different colors" ]
             ]
                 ++ [ primaryLabel [] [ text "Primary" ]
@@ -297,11 +300,11 @@ sectionForLabels =
         ]
 
 
-sectionForPlaceholder : Html msg
-sectionForPlaceholder =
+sectionForPlaceholders : Html msg
+sectionForPlaceholders =
     exampleContainer [ id "placeholder" ]
         [ example []
-            [ header [] [ text "Lines" ]
+            [ Header.header [] [ text "Lines" ]
             , p [] [ text "A placeholder can contain have lines of text" ]
             , placeholder []
                 [ line [] []
@@ -318,31 +321,31 @@ sectionForSegments : Html msg
 sectionForSegments =
     exampleContainer [ id "segments" ]
         [ example []
-            [ header [] [ text "Segment" ]
+            [ Header.header [] [ text "Segment" ]
             , p [] [ text "A segment of content" ]
             , segment [] [ wireframeParagraph ]
             ]
         , example []
-            [ header [] [ text "Vertical Segment" ]
+            [ Header.header [] [ text "Vertical Segment" ]
             , p [] [ text "A vertical segment formats content to be aligned as part of a vertical group" ]
             , verticalSegment [] [ wireframeParagraph ]
             , verticalSegment [] [ wireframeParagraph ]
             , verticalSegment [] [ wireframeParagraph ]
             ]
         , example []
-            [ header [] [ text "Disabled" ]
+            [ Header.header [] [ text "Disabled" ]
             , p [] [ text "A segment may show its content is disabled" ]
             , disabledSegment [] [ wireframeParagraph ]
             ]
         , example []
-            [ header [] [ text "Padded" ]
+            [ Header.header [] [ text "Padded" ]
             , p [] [ text "A segment can increase its padding" ]
             , paddedSegment [] [ wireframeParagraph ]
             ]
         , example []
             [ veryPaddedSegment [] [ wireframeParagraph ] ]
         , example []
-            [ header [] [ text "Basic" ]
+            [ Header.header [] [ text "Basic" ]
             , p [] [ text "A basic segment has no special formatting\n\n" ]
             , basicSegment []
                 [ p [] [ text "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo." ] ]
@@ -354,7 +357,7 @@ sectionForTexts : Html Msg
 sectionForTexts =
     exampleContainer [ id "text" ]
         [ example []
-            [ header [] [ text "Text" ]
+            [ Header.header [] [ text "Text" ]
             , p [] [ text "A text is always used inline and uses one color from the FUI color palette" ]
             , div []
                 [ text "This is "
@@ -398,7 +401,7 @@ sectionForTexts =
                 ]
             ]
         , example []
-            [ header [] [ text "Size" ]
+            [ Header.header [] [ text "Size" ]
             , p [] [ text "Text can vary in the same sizes as icons" ]
             , p [] [ text "Starting with ", miniText "mini", text " text" ]
             , p [] [ text "which turns into ", tinyText "tiny", text " text" ]
@@ -412,11 +415,11 @@ sectionForTexts =
         ]
 
 
-sectionForGrid : Html Msg
-sectionForGrid =
+sectionForGrids : Html Msg
+sectionForGrids =
     exampleContainer [ id "grid" ]
         [ example []
-            [ header [] [ text "Grids" ]
+            [ Header.header [] [ text "Grids" ]
             , p [] [ text "A grid is a structure with a long history used to align negative space in designs." ]
             , p [] [ text "Using a grid makes content appear to flow more naturally on your page." ]
             , let
@@ -459,7 +462,7 @@ sectionForMenus : Html Msg
 sectionForMenus =
     exampleContainer [ id "menu" ]
         [ example []
-            [ header [] [ text "Secondary Menu" ]
+            [ Header.header [] [ text "Secondary Menu" ]
             , p [] [ text "A menu can adjust its appearance to de-emphasize its contents" ]
             , secondaryMenu []
                 [ secondaryMenuActiveItem [] [ text "Home" ]
@@ -472,7 +475,7 @@ sectionForMenus =
                 ]
             ]
         , example []
-            [ header [] [ text "Vertical Menu" ]
+            [ Header.header [] [ text "Vertical Menu" ]
             , p [] [ text "A vertical menu displays elements vertically.." ]
             , verticalMenu { inverted = False, additionalStyles = [] } [] <|
                 [ verticalMenuActiveItem []
@@ -491,7 +494,7 @@ sectionForMenus =
                 ]
             ]
         , example []
-            [ header [] [ text "Link Item" ]
+            [ Header.header [] [ text "Link Item" ]
             , p [] [ text "A menu may contain a link item, or an item formatted as if it is a link." ]
             , verticalMenu { inverted = False, additionalStyles = [] } [] <|
                 [ verticalMenuLinkItem { inverted = False, additionalStyles = [] } [ href "http://www.google.com", Attributes.target "_blank", rel "noopener" ] [ text "Visit Google" ]
@@ -499,7 +502,7 @@ sectionForMenus =
                 ]
             ]
         , example []
-            [ header [] [ text "Inverted" ]
+            [ Header.header [] [ text "Inverted" ]
             , p [] [ text "A menu may have its colors inverted to show greater contrast" ]
             , verticalInvertedMenu []
                 [ verticalInvertedMenuItem [] [ text "Home" ]
@@ -514,7 +517,7 @@ sectionForTables : Html Msg
 sectionForTables =
     exampleContainer [ id "table" ]
         [ example []
-            [ header [] [ text "Table" ]
+            [ Header.header [] [ text "Table" ]
             , p [] [ text "A standard table" ]
             , celledTable []
                 [ thead []
@@ -544,7 +547,7 @@ sectionForTables =
                 ]
             ]
         , example []
-            [ header [] [ text "Striped" ]
+            [ Header.header [] [ text "Striped" ]
             , p [] [ text "A table can stripe alternate rows of content with a darker color to increase contrast" ]
             , stripedTable []
                 [ thead []
@@ -604,6 +607,53 @@ sectionForTables =
                         , td [] [ text "jhlilk22@yahoo.com" ]
                         , td [] [ text "No" ]
                         ]
+                    ]
+                ]
+            ]
+        ]
+
+
+sectionForCards : Html msg
+sectionForCards =
+    exampleContainer [ id "card" ]
+        [ example []
+            [ Header.header [] [ text "Header" ]
+            , p [] [ text "A card can contain a header" ]
+            , cards []
+                [ card []
+                    [ Card.content []
+                        [ Card.header [] [ text "Elliot Fu" ]
+                        , meta [] [ text "Friend" ]
+                        , description [] [ text "Elliot Fu is a film-maker from New York." ]
+                        ]
+                    ]
+                , card []
+                    [ Card.content []
+                        [ Card.header [] [ text "Veronika Ossi" ]
+                        , meta [] [ text "Friend" ]
+                        , description [] [ text "Veronika Ossi is a set designer living in New York who enjoys kittens, music, and partying." ]
+                        ]
+                    ]
+                , card []
+                    [ Card.content []
+                        [ Card.header [] [ text "Jenny Hess" ]
+                        , meta [] [ text "Friend" ]
+                        , description [] [ text "Jenny is a student studying Media Management at the New School" ]
+                        ]
+                    ]
+                ]
+            ]
+        , example []
+            [ Header.header [] [ text "Metadata" ]
+            , p [] [ text "A card can contain content metadata" ]
+            , card []
+                [ Card.content []
+                    [ Card.header [] [ text "Cute Dog" ]
+                    , meta []
+                        [ text "2 days ago "
+                        , a [] [ text "Animals" ]
+                        ]
+                    , wireframeParagraph
                     ]
                 ]
             ]
---
+++ src/UI/Card.elm
@@ -0,0 +1,239 @@
+module UI.Card exposing (card, cards, content, description, header, meta)
+
+import Css exposing (..)
+import Css.Global exposing (children, descendants, everything, selector)
+import Css.Prefix as Prefix
+import Css.Typography exposing (fomanticFont)
+import Html.Styled as Html exposing (Attribute, Html)
+
+
+cards : List (Attribute msg) -> List (Html msg) -> Html msg
+cards =
+    Html.styled Html.div <|
+        [ -- .ui.cards
+          Prefix.displayFlex
+        , margin2 (em -0.875) (em -0.5)
+        , Prefix.flexWrap "wrap"
+
+        -- .ui.cards > .card
+        , children
+            [ Css.Global.div
+                [ Prefix.displayFlex
+                , margin2 (em 0.875) (em 0.5)
+                , property "float" "none"
+                ]
+            ]
+
+        -- /* Clearing */
+        -- .ui.cards:after,
+        -- .ui.card:after {
+        --   display: block;
+        --   content: ' ';
+        --   height: 0;
+        --   clear: both;
+        --   overflow: hidden;
+        --   visibility: hidden;
+        ]
+
+
+cardBasis : List Style -> List (Attribute msg) -> List (Html msg) -> Html msg
+cardBasis additionalStyles =
+    Html.styled Html.div <|
+        [ -- .ui.cards > .card
+          -- .ui.card
+          maxWidth (pct 100)
+        , position relative
+        , Prefix.displayFlex
+        , property "-webkit-box-orient" "vertical"
+        , property "-webkit-box-direction" "normal"
+        , Prefix.flexDirection "column"
+        , width (px 290)
+        , minHeight zero
+        , property "background" "#FFFFFF"
+        , padding zero
+        , property "border" "none"
+        , borderRadius (rem 0.28571429)
+        , Prefix.boxShadow "0 1px 3px 0 #D4D4D5, 0 0 0 1px #D4D4D5"
+        , property "-webkit-transition" "-webkit-box-shadow 0.1s ease, -webkit-transform 0.1s ease"
+        , property "transition" "-webkit-box-shadow 0.1s ease, -webkit-transform 0.1s ease"
+        , property "transition" "box-shadow 0.1s ease, transform 0.1s ease"
+        , property "transition" "box-shadow 0.1s ease, transform 0.1s ease, -webkit-box-shadow 0.1s ease, -webkit-transform 0.1s ease"
+        , property "z-index" "''"
+        , property "word-wrap" "break-word"
+
+        -- .ui.card
+        , margin2 (em 1) zero
+
+        -- .ui.cards > .card a
+        -- .ui.card a
+        , descendants
+            [ Css.Global.a
+                [ cursor pointer ]
+            ]
+
+        -- .ui.card:first-child
+        -- , firstChild
+        --     [ marginTop zero ]
+        -- .ui.card:last-child
+        -- , firstChild
+        --     [ marginBottom zero ]
+        --
+        -- .ui.cards > .card
+        , fontSize (em 1)
+
+        -- .ui.cards > .card > :first-child
+        -- .ui.card > :first-child
+        , children
+            [ selector ":first-child"
+                [ borderRadius4 (rem 0.28571429) (rem 0.28571429) zero zero |> important
+                , property "border-top" "none" |> important
+                ]
+            ]
+
+        -- .ui.cards > .card > :last-child
+        -- .ui.card > :last-child
+        , children
+            [ selector ":last-child"
+                [ borderRadius4 zero zero (rem 0.28571429) (rem 0.28571429) |> important ]
+            ]
+
+        -- .ui.cards > .card > :only-child
+        -- .ui.card > :only-child
+        , children
+            [ selector ":only-child"
+                [ borderRadius (rem 0.28571429) |> important ]
+            ]
+        ]
+            ++ additionalStyles
+
+
+card : List (Attribute msg) -> List (Html msg) -> Html msg
+card =
+    cardBasis []
+
+
+content : List (Attribute msg) -> List (Html msg) -> Html msg
+content =
+    Html.styled Html.div <|
+        [ -- .ui.cards > .card > .content
+          -- .ui.card > .content
+          property "-webkit-box-flex" "1"
+        , property "-ms-flex-positive" "1"
+        , property "flex-grow" "1"
+        , property "border" "none"
+        , borderTop3 (px 1) solid (rgba 34 36 38 0.1)
+        , property "background" "none"
+        , margin zero
+        , padding2 (em 1) (em 1)
+        , Prefix.boxShadow "none"
+        , fontSize (em 1)
+        , borderRadius zero
+
+        -- .ui.cards > .card > .content:after
+        -- .ui.card > .content:after
+        , after
+            [ display block
+            , property "content" (qt " ")
+            , height zero
+            , property "clear" "both"
+            , overflow hidden
+            , visibility hidden
+            ]
+
+        -- .ui.cards > .card > .content p
+        -- .ui.card > .content p
+        , descendants
+            [ Css.Global.p
+                [ margin3 zero zero (em 0.5)
+
+                -- .ui.cards > .card > .content p:last-child,
+                -- .ui.card > .content p:last-child {
+                , lastChild
+                    [ marginBottom zero ]
+                ]
+
+            -- .ui.cards > .card > .content a:not(.ui)
+            -- .ui.card > .content a:not(.ui)
+            , Css.Global.a
+                [ property "color" "''"
+                , property "-webkit-transition" "color 0.1s ease"
+                , property "transition" "color 0.1s ease"
+
+                -- .ui.cards > .card > .content a:not(.ui):hover
+                -- .ui.card > .content a:not(.ui):hover
+                , hover
+                    [ property "color" "''" ]
+                ]
+            ]
+        ]
+
+
+header : List (Attribute msg) -> List (Html msg) -> Html msg
+header =
+    Html.styled Html.div <|
+        [ -- .ui.cards > .card > .content > .header
+          -- .ui.card > .content > .header
+          display block
+        , property "margin" "''"
+        , fontFamilies fomanticFont
+        , color (rgba 0 0 0 0.85)
+
+        -- .ui.cards > .card > .content > .header:not(.ui)
+        -- .ui.card > .content > .header:not(.ui)
+        , fontWeight bold
+        , fontSize (em 1.28571429)
+        , marginTop (em -0.21425)
+        , lineHeight (em 1.28571429)
+        ]
+
+
+meta : List (Attribute msg) -> List (Html msg) -> Html msg
+meta =
+    Html.styled Html.div <|
+        [ -- .ui.cards > .card .meta
+          -- .ui.card .meta
+          fontSize (em 1)
+        , color (rgba 0 0 0 0.4)
+
+        -- .ui.cards > .card .meta *
+        -- .ui.card .meta *
+        , descendants
+            [ everything
+                [ marginRight (em 0.3) ]
+
+            -- .ui.cards > .card .meta :last-child
+            -- .ui.card .meta :last-child
+            , selector ":last-child"
+                [ marginRight zero
+                ]
+            ]
+
+        -- .ui.cards > .card .meta > a:not(.ui)
+        -- .ui.card .meta > a:not(.ui)
+        , children
+            [ Css.Global.a
+                [ color (rgba 0 0 0 0.4)
+                , hover
+                    [ -- .ui.cards > .card .meta > a:not(.ui):hover
+                      -- .ui.card .meta > a:not(.ui):hover
+                      color (rgba 0 0 0 0.87)
+                    ]
+                ]
+            ]
+        ]
+
+
+description : List (Attribute msg) -> List (Html msg) -> Html msg
+description =
+    Html.styled Html.div <|
+        [ -- .ui.cards > .card > .content > .meta + .description
+          -- .ui.cards > .card > .content > .header + .description
+          -- .ui.card > .content > .meta + .description
+          -- .ui.card > .content > .header + .description
+          marginTop (em 0.5)
+
+        -- .ui.cards > .card > .content > .description
+        -- .ui.card > .content > .description
+        , property "clear" "both"
+        , color (rgba 0 0 0 0.68)
+        ]
