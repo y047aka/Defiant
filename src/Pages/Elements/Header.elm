99f2d0ba432543d3e9ac79e2e7bc00097e5151a7
Yoshitaka Totsuka <yoshitaka.totsuka@siiibo.com>
2022-08-11 06:49:26 +0900

    Apply configAndPreview

--- src/Pages/Elements/Header.elm
+++ src/Pages/Elements/Header.elm
@@ -1,60 +1,108 @@
-module Pages.Elements.Header exposing (page)
+module Pages.Elements.Header exposing (Model, Msg, page)
 
-import Html.Styled as Html exposing (Html, text)
-import Page exposing (Page)
+import Data exposing (PresetColor(..), Size(..), sizeFromString, sizeToString)
+import Html.Styled as Html exposing (Html, div, option, select, text)
+import Html.Styled.Attributes exposing (selected, value)
+import Html.Styled.Events exposing (onInput)
+import Page
 import Request exposing (Request)
 import Shared
-import UI.Example exposing (example, wireframeShortParagraph)
+import UI.Example exposing (wireframeShortParagraph)
 import UI.Header as Header exposing (..)
 import UI.Icon exposing (icon)
+import View.ConfigAndPreview exposing (configAndPreview)
 
 
-page : Shared.Model -> Request -> Page
+page : Shared.Model -> Request -> Page.With Model Msg
 page shared _ =
-    Page.static
-        { view =
-            { title = "Header"
-            , body = view { shared = shared }
-            }
+    Page.sandbox
+        { init = init
+        , update = update
+        , view =
+            \model ->
+                { title = "Header"
+                , body = view shared model
+                }
         }
+
+
+
+-- INIT
 
 
 type alias Model =
-    { shared : Shared.Model }
+    { size : Size }
 
 
-view : Model -> List (Html msg)
-view { shared } =
+init : Model
+init =
+    { size = Medium }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = ChangeSize Size
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        ChangeSize size ->
+            { model | size = size }
+
+
+
+-- VIEW
+
+
+view : Shared.Model -> Model -> List (Html Msg)
+view { theme } model =
     let
         options =
-            { theme = shared.theme }
+            { theme = theme }
     in
-    [ example
-        { title = "Content Headers"
-        , description = "Headers may be oriented to give the importance of a section in the context of the content that surrounds it"
-        }
-        [ massiveHeader options [] [ text "Massive Header" ]
-        , wireframeShortParagraph
-        , hugeHeader options [] [ text "Huge Header" ]
-        , wireframeShortParagraph
-        , bigHeader options [] [ text "Big Header" ]
-        , wireframeShortParagraph
-        , largeHeader options [] [ text "Large Header" ]
-        , wireframeShortParagraph
-        , mediumHeader options [] [ text "Medium Header" ]
-        , wireframeShortParagraph
-        , smallHeader options [] [ text "Small Header" ]
-        , wireframeShortParagraph
-        , tinyHeader options [] [ text "Tiny Header" ]
-        , wireframeShortParagraph
-        , miniHeader options [] [ text "Mini Header" ]
-        , wireframeShortParagraph
+    [ configAndPreview { title = "Content Headers" }
+        (div [] <|
+            [ case model.size of
+                Massive ->
+                    massiveHeader options [] [ text "Massive Header" ]
+
+                Huge ->
+                    hugeHeader options [] [ text "Huge Header" ]
+
+                Big ->
+                    bigHeader options [] [ text "Big Header" ]
+
+                Large ->
+                    largeHeader options [] [ text "Large Header" ]
+
+                Medium ->
+                    mediumHeader options [] [ text "Medium Header" ]
+
+                Small ->
+                    smallHeader options [] [ text "Small Header" ]
+
+                Tiny ->
+                    tinyHeader options [] [ text "Tiny Header" ]
+
+                Mini ->
+                    miniHeader options [] [ text "Mini Header" ]
+            , wireframeShortParagraph
+            ]
+        )
+        [ { label = "Size"
+          , description = "Text can vary in the same sizes as icons"
+          , content =
+                select [ onInput (sizeFromString >> Maybe.withDefault model.size >> ChangeSize) ] <|
+                    List.map (\state -> option [ value (sizeToString state), selected (model.size == state) ] [ text (sizeToString state) ])
+                        [ Massive, Huge, Big, Large, Medium, Small, Tiny, Mini ]
+          }
         ]
-    , example
-        { title = "Icon Headers"
-        , description = "A header can be formatted to emphasize an icon"
-        }
-        [ iconHeader options
+    , configAndPreview { title = "Icon Headers" }
+        (iconHeader options
             []
             [ icon [] "fas fa-cogs"
             , iconHeaderContent []
@@ -62,16 +110,15 @@ view { shared } =
                 , subHeader options [] [ text "Manage your account settings and set e-mail preferences." ]
                 ]
             ]
-        ]
-    , example
-        { title = "Subheader"
-        , description = "Headers may contain subheaders"
-        }
-        [ Header.header options
+        )
+        []
+    , configAndPreview { title = "Subheader" }
+        (Header.header options
             []
             [ text "Account Settings"
             , subHeader options [] [ text "Manage your account settings and set e-mail preferences." ]
             , wireframeShortParagraph
             ]
-        ]
+        )
+        []
     ]
