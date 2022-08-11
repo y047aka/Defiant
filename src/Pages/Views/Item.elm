0ccfe8dea61252ccd6c30d175a5261cd434b0c32
Yoshitaka Totsuka <yoshitaka.totsuka@siiibo.com>
2022-08-12 01:54:01 +0900

    Apply configAndPreview

--- src/Pages/Views/Item.elm
+++ src/Pages/Views/Item.elm
@@ -1,161 +1,178 @@
-module Pages.Views.Item exposing (page)
+module Pages.Views.Item exposing (Model, Msg, page)
 
-import Html.Styled as Html exposing (Html, p, span, text)
-import Html.Styled.Attributes exposing (src)
-import Page exposing (Page)
+import Html.Styled as Html exposing (Html, span, text)
+import Html.Styled.Attributes exposing (checked, for, id, src, type_)
+import Html.Styled.Events exposing (onClick)
+import Page
 import Request exposing (Request)
 import Shared
-import UI.Example exposing (example, wireframeShortParagraph)
-import UI.Icon exposing (icon)
-import UI.Image exposing (image, smallImage, tinyImage)
+import UI.Checkbox as Checkbox exposing (checkbox)
+import UI.Example exposing (wireframeShortParagraph)
+import UI.Image exposing (image)
 import UI.Item as Item exposing (..)
+import View.ConfigAndPreview exposing (configAndPreview)
 
 
-page : Shared.Model -> Request -> Page
+page : Shared.Model -> Request -> Page.With Model Msg
 page _ _ =
-    Page.static
-        { view =
-            { title = "Item"
-            , body = view
-            }
+    Page.sandbox
+        { init = init
+        , update = update
+        , view =
+            \model ->
+                { title = "Segment"
+                , body = view model
+                }
         }
 
 
-view : List (Html msg)
-view =
-    [ example
-        { title = "Metadata"
-        , description = "An item can contain content metadata"
-        }
-        [ items [] <|
-            List.repeat 2
-                (Item.item []
-                    [ image [ src "/static/images/wireframe/image.png" ] []
+
+-- INIT
+
+
+type alias Model =
+    { hasImage : Bool
+    , hasHeader : Bool
+    , hasMetadata : Bool
+    , hasDescription : Bool
+    , hasExtraContent : Bool
+    }
+
+
+init : Model
+init =
+    { hasImage = True
+    , hasHeader = True
+    , hasMetadata = True
+    , hasDescription = True
+    , hasExtraContent = True
+    }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = ToggleHasImage
+    | ToggleHasHeader
+    | ToggleHasMetadata
+    | ToggleHasDescription
+    | ToggleHasExtraContent
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        ToggleHasImage ->
+            { model | hasImage = not model.hasImage }
+
+        ToggleHasHeader ->
+            { model | hasHeader = not model.hasHeader }
+
+        ToggleHasMetadata ->
+            { model | hasMetadata = not model.hasMetadata }
+
+        ToggleHasDescription ->
+            { model | hasDescription = not model.hasDescription }
+
+        ToggleHasExtraContent ->
+            { model | hasExtraContent = not model.hasExtraContent }
+
+
+view : Model -> List (Html Msg)
+view model =
+    [ configAndPreview { title = "Metadata" }
+        (let
+            item { header, metadata } =
+                Item.item []
+                    [ if model.hasImage then
+                        image [ src "/static/images/wireframe/image.png" ] []
+
+                      else
+                        text ""
                     , Item.content []
-                        { header = [ text "Header" ]
-                        , meta = [ span [] [ text "Description" ] ]
-                        , description = [ wireframeShortParagraph ]
-                        , extra = [ text "Additional Details" ]
+                        { header =
+                            if model.hasHeader then
+                                [ text header ]
+
+                            else
+                                []
+                        , meta =
+                            if model.hasMetadata then
+                                [ span [] [ text metadata ] ]
+
+                            else
+                                []
+                        , description =
+                            if model.hasDescription then
+                                [ wireframeShortParagraph ]
+
+                            else
+                                []
+                        , extra =
+                            if model.hasExtraContent then
+                                [ text "Extra Content" ]
+
+                            else
+                                []
                         }
                     ]
-                )
-        ]
-    , example
-        { title = "Image"
-        , description = "An item can contain an image"
-        }
-        [ dividedItems [] <|
-            List.repeat 3
-                (Item.item []
-                    [ image [ src "/static/images/wireframe/image.png" ] [] ]
-                )
-        ]
-    , example
-        { title = "Content"
-        , description = "An item can contain content"
-        }
-        [ dividedItems [] <|
-            List.map
-                (\{ content } ->
-                    Item.item []
-                        [ tinyImage [ src "/static/images/wireframe/image.png" ] []
-                        , middleAlignedContent [] [ text content ]
-                        ]
-                )
-                [ { content = "Content A" }
-                , { content = "Content B" }
-                , { content = "Content C" }
-                ]
-        ]
-    , example
-        { title = "Header"
-        , description = "An item can contain a header"
-        }
-        [ items [] <|
-            List.map
-                (\{ title } ->
-                    Item.item []
-                        [ tinyImage [ src "/static/images/wireframe/image.png" ] []
-                        , middleAlignedContent [] [ Item.header [] [ text title ] ]
-                        ]
-                )
-                [ { title = "12 Years a Slave" }
-                , { title = "My Neighbor Totoro" }
-                , { title = "Watchmen" }
-                ]
-        ]
-    , example
-        { title = "Metadata"
-        , description = "An item can contain content metadata"
-        }
-        [ items [] <|
-            List.map
-                (\plan ->
-                    Item.item []
-                        [ smallImage [ src "/static/images/wireframe/image.png" ] []
-                        , Item.content []
-                            { header = [ text plan.title ]
-                            , meta =
-                                [ span [] [ text plan.price ]
-                                , span [] [ text plan.stay ]
-                                ]
-                            , description = [ wireframeShortParagraph ]
-                            , extra = []
-                            }
-                        ]
-                )
-                [ { title = "Arrowhead Valley Camp"
-                  , price = "$1200"
-                  , stay = "1 Month"
-                  }
-                , { title = "Buck's Homebrew Stayaway"
-                  , price = "$1000"
-                  , stay = "2 Weeks"
-                  }
-                , { title = "Astrology Camp"
-                  , price = "$1600"
-                  , stay = "6 Weeks"
-                  }
-                ]
-        ]
-    , example
-        { title = "Description"
-        , description = "An item can contain a description with a single or multiple paragraphs"
-        }
-        [ items []
-            [ Item.item []
-                [ smallImage [ src "/static/images/wireframe/image.png" ] []
-                , Item.content []
-                    { header = [ text "Cute Dog" ]
-                    , meta = []
-                    , description =
-                        [ p [] [ text "Cute dogs come in a variety of shapes and sizes. Some cute dogs are cute for their adorable faces, others for their tiny stature, and even others for their massive size." ]
-                        , p [] [ text "Many people also have their own barometers for what makes a cute dog." ]
-                        ]
-                    , extra = []
-                    }
-                ]
+         in
+         items []
+            [ item
+                { header = "12 Years a Slave"
+                , metadata = "Union Square 14"
+                }
+            , item
+                { header = "My Neighbor Totoro"
+                , metadata = "IFC Cinema"
+                }
+            , item
+                { header = "Watchmen"
+                , metadata = "IFC"
+                }
             ]
-        ]
-    , example
-        { title = "Extra Content"
-        , description = "An item can contain extra content meant to be formatted separately from the main content"
-        }
-        [ items []
-            [ Item.item []
-                [ Item.content []
-                    { header = [ text "Cute Dog" ]
-                    , meta = []
-                    , description =
-                        [ wireframeShortParagraph
-                        , wireframeShortParagraph
-                        ]
-                    , extra =
-                        [ icon [] "fas fa-check"
-                        , text "121 Votes"
-                        ]
-                    }
-                ]
-            ]
+        )
+        [ { label = "Image"
+          , description = "An item can contain an image"
+          , content =
+                checkbox []
+                    [ Checkbox.input [ id "image", type_ "checkbox", checked model.hasImage, onClick ToggleHasImage ] []
+                    , Checkbox.label [ for "image" ] [ text "Image" ]
+                    ]
+          }
+        , { label = "Header"
+          , description = "An item can contain a header"
+          , content =
+                checkbox []
+                    [ Checkbox.input [ id "header", type_ "checkbox", checked model.hasHeader, onClick ToggleHasHeader ] []
+                    , Checkbox.label [ for "header" ] [ text "Header" ]
+                    ]
+          }
+        , { label = "Metadata"
+          , description = "An item can contain content metadata"
+          , content =
+                checkbox []
+                    [ Checkbox.input [ id "metadata", type_ "checkbox", checked model.hasMetadata, onClick ToggleHasMetadata ] []
+                    , Checkbox.label [ for "metadata" ] [ text "Metadata" ]
+                    ]
+          }
+        , { label = "Description"
+          , description = "An item can contain a description with a single or multiple paragraphs"
+          , content =
+                checkbox []
+                    [ Checkbox.input [ id "description", type_ "checkbox", checked model.hasDescription, onClick ToggleHasDescription ] []
+                    , Checkbox.label [ for "description" ] [ text "Description" ]
+                    ]
+          }
+        , { label = "ExtraContent"
+          , description = "An item can contain content ExtraContent"
+          , content =
+                checkbox []
+                    [ Checkbox.input [ id "extra_content", type_ "checkbox", checked model.hasExtraContent, onClick ToggleHasExtraContent ] []
+                    , Checkbox.label [ for "extra_content" ] [ text "ExtraContent" ]
+                    ]
+          }
         ]
     ]
