a8e219560ea3b4e009a6ff0d5805ec5cc17292b0
Yoshitaka Totsuka <y047aka@gmail.com>
2022-08-16 13:57:48 +0900

    Update `Typography`

--- src/Css/Typography.elm
+++ src/Css/Typography.elm
@@ -1,7 +1,7 @@
 module Css.Typography exposing
     ( Typography, init
     , typography
-    , setFontFamilies, setFontWeight
+    , setFontFamilies, setFontSize, setFontStyle, setFontWeight, setLineHeight, setLetterSpacing, setTextDecoration, setTextTransform
     , default, bold
     )
 
@@ -9,26 +9,37 @@ module Css.Typography exposing
 
 @docs Typography, init
 @docs typography
-@docs setFontFamilies, setFontWeight
+@docs setFontFamilies, setFontSize, setFontStyle, setFontWeight, setLineHeight, setLetterSpacing, setTextDecoration, setTextTransform
 
 @docs default, bold
-@docs fomanticFontFamilies
 
 -}
 
-import Css exposing (Compatible, FontWeight, Style, batch, fontFamilies, fontWeight, qt)
+import Css exposing (FontSize, FontStyle, FontWeight, Length, LengthOrNumber, Style, TextDecorationLine, TextTransform, batch, fontFamilies, fontSize, fontStyle, fontWeight, letterSpacing, lineHeight, qt, textDecoration, textTransform)
 
 
 type alias Typography =
     { fontFamilies : List String
-    , fontWeight : Maybe { value : String, fontWeight : Compatible }
+    , fontSize : Maybe (FontSize {})
+    , fontStyle : Maybe (FontStyle {})
+    , fontWeight : Maybe (FontWeight {})
+    , lineHeight : Maybe (LengthOrNumber {})
+    , letterSpacing : Maybe (Length {} {})
+    , textDecoration : Maybe (TextDecorationLine {})
+    , textTransform : Maybe (TextTransform {})
     }
 
 
 init : Typography
 init =
     { fontFamilies = []
+    , fontSize = Nothing
+    , fontStyle = Nothing
     , fontWeight = Nothing
+    , lineHeight = Nothing
+    , letterSpacing = Nothing
+    , textDecoration = Nothing
+    , textTransform = Nothing
     }
 
 
@@ -39,8 +50,26 @@ typography t =
 
       else
         batch []
+    , t.fontSize
+        |> Maybe.map fontSize
+        |> Maybe.withDefault (batch [])
+    , t.fontStyle
+        |> Maybe.map fontStyle
+        |> Maybe.withDefault (batch [])
     , t.fontWeight
         |> Maybe.map fontWeight
+        |> Maybe.withDefault (batch [])
+    , t.lineHeight
+        |> Maybe.map lineHeight
+        |> Maybe.withDefault (batch [])
+    , t.letterSpacing
+        |> Maybe.map letterSpacing
+        |> Maybe.withDefault (batch [])
+    , t.textDecoration
+        |> Maybe.map textDecoration
+        |> Maybe.withDefault (batch [])
+    , t.textTransform
+        |> Maybe.map textTransform
         |> Maybe.withDefault (batch [])
     ]
         |> batch
@@ -51,9 +80,39 @@ setFontFamilies families t =
     { t | fontFamilies = families }
 
 
+setFontSize : FontSize a -> Typography -> Typography
+setFontSize { value, fontSize } t =
+    { t | fontSize = Just { value = value, fontSize = fontSize } }
+
+
+setFontStyle : FontStyle a -> Typography -> Typography
+setFontStyle { value, fontStyle } t =
+    { t | fontStyle = Just { value = value, fontStyle = fontStyle } }
+
+
 setFontWeight : FontWeight a -> Typography -> Typography
 setFontWeight { value, fontWeight } t =
     { t | fontWeight = Just { value = value, fontWeight = fontWeight } }
+
+
+setLineHeight : LengthOrNumber compatible -> Typography -> Typography
+setLineHeight { value, lengthOrNumber } t =
+    { t | lineHeight = Just { value = value, lengthOrNumber = lengthOrNumber } }
+
+
+setLetterSpacing : Length compatible unit -> Typography -> Typography
+setLetterSpacing { value, length, numericValue, unitLabel } t =
+    { t | letterSpacing = Just { value = value, length = length, numericValue = numericValue, units = {}, unitLabel = unitLabel } }
+
+
+setTextDecoration : TextDecorationLine a -> Typography -> Typography
+setTextDecoration { value, textDecorationLine } t =
+    { t | textDecoration = Just { value = value, textDecorationLine = textDecorationLine } }
+
+
+setTextTransform : TextTransform compatible -> Typography -> Typography
+setTextTransform { value, textTransform } t =
+    { t | textTransform = Just { value = value, textTransform = textTransform } }
 
 
 default : Typography
