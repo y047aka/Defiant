1a3caa6c6b78537cec4904ea82011ced4306f573
Yoshitaka Totsuka <y047aka@gmail.com>
2024-09-21 23:55:33 +0900

    fix: restore `Headless.Text`

---
+++ package/src/Headless/Text.elm
@@ -0,0 +1,55 @@
+module Headless.Text exposing
+    ( TextProps, TextAs(..), defaultTextProps
+    , text
+    )
+
+{-|
+
+@docs TextProps, TextAs, defaultTextProps
+@docs text
+
+-}
+
+import Html.Styled as Html exposing (Attribute, Html)
+
+
+type alias TextProps =
+    { textAs : TextAs
+    }
+
+
+type TextAs
+    = Span
+    | Div
+    | Label
+    | P
+
+
+defaultTextProps : TextProps
+defaultTextProps =
+    { textAs = Span }
+
+
+text : TextProps -> String -> Html msg
+text props string =
+    Html.styled (textAsToTag props.textAs) [] [] [ Html.text string ]
+
+
+
+-- PRIVATE HELPERS
+
+
+textAsToTag : TextAs -> (List (Attribute msg) -> List (Html msg) -> Html msg)
+textAsToTag textAs =
+    case textAs of
+        Span ->
+            Html.span
+
+        Div ->
+            Html.div
+
+        Label ->
+            Html.label
+
+        P ->
+            Html.p
