04dd5d719c2537f4ffd1c0cc759cac576c6e937d
Yoshitaka Totsuka <y047aka@gmail.com>
2024-09-26 17:25:46 +0900

    example: update Index

--- example/app/Route/Index.elm
+++ example/app/Route/Index.elm
@@ -1,15 +1,18 @@
 module Route.Index exposing (ActionData, Data, Model, Msg, route)
 
 import BackendTask exposing (BackendTask)
+import BackendTask.Glob as Glob
 import FatalError exposing (FatalError)
 import Head
 import Head.Seo as Seo
-import Html.Styled as Html
+import Html.Styled as Html exposing (Attribute, Html, a, h1, li, text, ul)
+import Html.Styled.Attributes as Attributes
 import Pages.Url
 import PagesMsg exposing (PagesMsg)
-import Route
+import Route exposing (Route)
 import RouteBuilder exposing (App, StatelessRoute)
 import Shared
+import String.Extra
 import UrlPath
 import View exposing (View)
 
@@ -44,15 +47,46 @@ route =
 
 
 type alias Data =
-    { message : String
-    }
+    List ( Route, ArticleMetadata )
 
 
 data : BackendTask FatalError Data
 data =
-    BackendTask.succeed Data
-        |> BackendTask.andMap
-            (BackendTask.succeed "Hello!")
+    allMetadata
+
+
+type alias File =
+    { filePath : String
+    , name : String
+    }
+
+
+componentPagesGlob : BackendTask FatalError (List File)
+componentPagesGlob =
+    Glob.succeed File
+        |> Glob.captureFilePath
+        |> Glob.match (Glob.literal "app/Route/")
+        |> Glob.capture Glob.wildcard
+        |> Glob.match (Glob.literal ".elm")
+        |> Glob.toBackendTask
+
+
+allMetadata : BackendTask FatalError (List ( Route, ArticleMetadata ))
+allMetadata =
+    componentPagesGlob
+        |> BackendTask.map
+            (List.filterMap
+                (\{ name } ->
+                    Route.segmentsToRoute [ String.Extra.dasherize name |> String.dropLeft 1 ]
+                        |> Maybe.map (\route_ -> ( route_, { title = name, slug = name } ))
+                )
+            )
+
+
+type alias ArticleMetadata =
+    { title : String
+    , slug : String
+    }
 
 
 head :
@@ -86,9 +120,24 @@ view :
 view app shared =
     { title = "elm-pages is running"
     , body =
-        [ Html.h1 [] [ Html.text "elm-pages is up and running!" ]
-        , Html.p []
-            [ Html.text <| "The message is: " ++ app.data.message
-            ]
+        [ h1 [] [ text "elm-pages is up and running!" ]
+        , ul []
+            (List.map
+                (\( route_, page ) ->
+                    li [] [ link route_ [] [ text page.title ] ]
+                )
+                app.data
+            )
         ]
     }
+
+
+link : Route -> List (Attribute msg) -> List (Html msg) -> Html msg
+link route_ attrs children =
+    Route.toLink
+        (\anchorAttrs ->
+            a
+                (List.map Attributes.fromUnstyled anchorAttrs ++ attrs)
+                children
+        )
+        route_
