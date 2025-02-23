49f565f6b547248358c34d81820f45d62b42a5b7
Yoshitaka Totsuka <y047aka@gmail.com>
2025-02-24 01:01:28 +0900

    feat: Serendieのリファレンストークンを参照

---
+++ package/src/DesignToken/Tokens/Reference/Dimension.elm
@@ -0,0 +1,105 @@
+module DesignToken.Tokens.Reference.Dimension exposing (sd)
+
+
+sd =
+    { reference =
+        { dimension =
+            { scale =
+                { dimension0 =
+                    { value = "0px"
+                    , type_ = "dimension"
+                    }
+                , dimension1 =
+                    { value = "1px"
+                    , type_ = "dimension"
+                    }
+                , dimension2 =
+                    { value = "2px"
+                    , type_ = "dimension"
+                    }
+                , dimension3 =
+                    { value = "4px"
+                    , type_ = "dimension"
+                    }
+                , dimension4 =
+                    { value = "8px"
+                    , type_ = "dimension"
+                    }
+                , dimension5 =
+                    { value = "12px"
+                    , type_ = "dimension"
+                    }
+                , dimension6 =
+                    { value = "16px"
+                    , type_ = "dimension"
+                    }
+                , dimension7 =
+                    { value = "20px"
+                    , type_ = "dimension"
+                    }
+                , dimension8 =
+                    { value = "24px"
+                    , type_ = "dimension"
+                    }
+                , dimension9 =
+                    { value = "28px"
+                    , type_ = "dimension"
+                    }
+                , dimension10 =
+                    { value = "32px"
+                    , type_ = "dimension"
+                    }
+                , dimension11 =
+                    { value = "36px"
+                    , type_ = "dimension"
+                    }
+                , dimension12 =
+                    { value = "40px"
+                    , type_ = "dimension"
+                    }
+                , dimension13 =
+                    { value = "48px"
+                    , type_ = "dimension"
+                    }
+                , dimension14 =
+                    { value = "56px"
+                    , type_ = "dimension"
+                    }
+                , dimension15 =
+                    { value = "64px"
+                    , type_ = "dimension"
+                    }
+                , dimension16 =
+                    { value = "72px"
+                    , type_ = "dimension"
+                    }
+                , dimension17 =
+                    { value = "80px"
+                    , type_ = "dimension"
+                    }
+                , dimension18 =
+                    { value = "96px"
+                    , type_ = "dimension"
+                    }
+                }
+            , breakpoint =
+                { small =
+                    { value = "640px"
+                    , type_ = "dimension"
+                    }
+                , medium =
+                    { value = "768px"
+                    , type_ = "dimension"
+                    }
+                , large =
+                    { value = "1024px"
+                    , type_ = "dimension"
+                    }
+                , extraLarge =
+                    { value = "1280px"
+                    , type_ = "dimension"
+                    }
+                }
+            }
+        }
+    }
