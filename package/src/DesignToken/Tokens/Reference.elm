ecaa705c1b399be23005829d6c1ffd7af2f6854c
Yoshitaka Totsuka <y047aka@gmail.com>
2025-02-24 02:11:26 +0900

    feat: トークンを `Reference.elm` に集約

---
+++ package/src/DesignToken/Tokens/Reference.elm
@@ -0,0 +1,742 @@
+module DesignToken.Tokens.Reference exposing (sd)
+
+
+sd =
+    { reference =
+        { color = color
+        , dimension = dimension
+        , elevation = elevation
+        , typography = typography
+        }
+    }
+
+
+
+-- COLOR
+
+
+color =
+    { scale =
+        { white1000 =
+            { value = "#FFFFFF"
+            , type_ = "color"
+            }
+        , black1000 =
+            { value = "#000000"
+            , type_ = "color"
+            }
+        , transparency5 =
+            { value = "#0000000D"
+            , type_ = "color"
+            }
+        , transparency20 =
+            { value = "#00000033"
+            , type_ = "color"
+            }
+        , transparency60 =
+            { value = "#FFFFFF99"
+            , type_ = "color"
+            }
+        , gray100 =
+            { value = "#EFEEEB"
+            , type_ = "color"
+            }
+        , gray200 =
+            { value = "#D9D8D3"
+            , type_ = "color"
+            }
+        , gray300 =
+            { value = "#C8C7C2"
+            , type_ = "color"
+            }
+        , gray400 =
+            { value = "#AFAEAA"
+            , type_ = "color"
+            }
+        , gray500 =
+            { value = "#8C8B87"
+            , type_ = "color"
+            }
+        , gray600 =
+            { value = "#696966"
+            , type_ = "color"
+            }
+        , gray700 =
+            { value = "#525250"
+            , type_ = "color"
+            }
+        , gray800 =
+            { value = "#3F3F3D"
+            , type_ = "color"
+            }
+        , gray900 =
+            { value = "#31312F"
+            , type_ = "color"
+            }
+        , gray1000 =
+            { value = "#232322"
+            , type_ = "color"
+            }
+        , red100 =
+            { value = "#FCEBEA"
+            , type_ = "color"
+            }
+        , red200 =
+            { value = "#FCD6D3"
+            , type_ = "color"
+            }
+        , red300 =
+            { value = "#FDB9B4"
+            , type_ = "color"
+            }
+        , red400 =
+            { value = "#FF8F8F"
+            , type_ = "color"
+            }
+        , red500 =
+            { value = "#F84258"
+            , type_ = "color"
+            }
+        , red600 =
+            { value = "#CE0037"
+            , type_ = "color"
+            }
+        , red700 =
+            { value = "#A4002A"
+            , type_ = "color"
+            }
+        , red800 =
+            { value = "#81001F"
+            , type_ = "color"
+            }
+        , red900 =
+            { value = "#650417"
+            , type_ = "color"
+            }
+        , red1000 =
+            { value = "#49060E"
+            , type_ = "color"
+            }
+        , chestnut100 =
+            { value = "#FAECE6"
+            , type_ = "color"
+            }
+        , chestnut200 =
+            { value = "#F7D8C9"
+            , type_ = "color"
+            }
+        , chestnut300 =
+            { value = "#F7C6B0"
+            , type_ = "color"
+            }
+        , chestnut400 =
+            { value = "#F49567"
+            , type_ = "color"
+            }
+        , chestnut500 =
+            { value = "#E26324"
+            , type_ = "color"
+            }
+        , chestnut600 =
+            { value = "#AB4919"
+            , type_ = "color"
+            }
+        , chestnut700 =
+            { value = "#803510"
+            , type_ = "color"
+            }
+        , chestnut800 =
+            { value = "#692C0D"
+            , type_ = "color"
+            }
+        , chestnut900 =
+            { value = "#50230D"
+            , type_ = "color"
+            }
+        , chestnut1000 =
+            { value = "#371B0B"
+            , type_ = "color"
+            }
+        , beige100 =
+            { value = "#F7EDE2"
+            , type_ = "color"
+            }
+        , beige200 =
+            { value = "#F2DBC0"
+            , type_ = "color"
+            }
+        , beige300 =
+            { value = "#EDC18A"
+            , type_ = "color"
+            }
+        , beige400 =
+            { value = "#DAA358"
+            , type_ = "color"
+            }
+        , beige500 =
+            { value = "#AF8245"
+            , type_ = "color"
+            }
+        , beige600 =
+            { value = "#846132"
+            , type_ = "color"
+            }
+        , beige700 =
+            { value = "#684C26"
+            , type_ = "color"
+            }
+        , beige800 =
+            { value = "#503A1C"
+            , type_ = "color"
+            }
+        , beige900 =
+            { value = "#3E2D17"
+            , type_ = "color"
+            }
+        , beige1000 =
+            { value = "#2B2013"
+            , type_ = "color"
+            }
+        , yellow100 =
+            { value = "#FEEDBE"
+            , type_ = "color"
+            }
+        , yellow200 =
+            { value = "#FFE885"
+            , type_ = "color"
+            }
+        , yellow300 =
+            { value = "#FFE600"
+            , type_ = "color"
+            }
+        , yellow400 =
+            { value = "#EDD202"
+            , type_ = "color"
+            }
+        , yellow500 =
+            { value = "#AC9301"
+            , type_ = "color"
+            }
+        , yellow600 =
+            { value = "#857100"
+            , type_ = "color"
+            }
+        , yellow700 =
+            { value = "#5F5100"
+            , type_ = "color"
+            }
+        , yellow800 =
+            { value = "#4A3E00"
+            , type_ = "color"
+            }
+        , yellow900 =
+            { value = "#3A3000"
+            , type_ = "color"
+            }
+        , yellow1000 =
+            { value = "#292200"
+            , type_ = "color"
+            }
+        , green100 =
+            { value = "#CAF9E6"
+            , type_ = "color"
+            }
+        , green200 =
+            { value = "#AEEAD5"
+            , type_ = "color"
+            }
+        , green300 =
+            { value = "#98E1C8"
+            , type_ = "color"
+            }
+        , green400 =
+            { value = "#6ED0AE"
+            , type_ = "color"
+            }
+        , green500 =
+            { value = "#40A683"
+            , type_ = "color"
+            }
+        , green600 =
+            { value = "#2C755C"
+            , type_ = "color"
+            }
+        , green700 =
+            { value = "#266751"
+            , type_ = "color"
+            }
+        , green800 =
+            { value = "#184737"
+            , type_ = "color"
+            }
+        , green900 =
+            { value = "#12372B"
+            , type_ = "color"
+            }
+        , green1000 =
+            { value = "#0D271E"
+            , type_ = "color"
+            }
+        , skyBlue100 =
+            { value = "#D9F6FC"
+            , type_ = "color"
+            }
+        , skyBlue200 =
+            { value = "#C3EFF4"
+            , type_ = "color"
+            }
+        , skyBlue300 =
+            { value = "#9CE6EC"
+            , type_ = "color"
+            }
+        , skyBlue400 =
+            { value = "#64CCD3"
+            , type_ = "color"
+            }
+        , skyBlue500 =
+            { value = "#00A3AF"
+            , type_ = "color"
+            }
+        , skyBlue600 =
+            { value = "#00757E"
+            , type_ = "color"
+            }
+        , skyBlue700 =
+            { value = "#015C63"
+            , type_ = "color"
+            }
+        , skyBlue800 =
+            { value = "#01474C"
+            , type_ = "color"
+            }
+        , skyBlue900 =
+            { value = "#02373C"
+            , type_ = "color"
+            }
+        , skyBlue1000 =
+            { value = "#04272A"
+            , type_ = "color"
+            }
+        , blue100 =
+            { value = "#EFF2FC"
+            , type_ = "color"
+            }
+        , blue200 =
+            { value = "#D7DEFB"
+            , type_ = "color"
+            }
+        , blue300 =
+            { value = "#BFCEFC"
+            , type_ = "color"
+            }
+        , blue400 =
+            { value = "#8FAEFE"
+            , type_ = "color"
+            }
+        , blue500 =
+            { value = "#428CFE"
+            , type_ = "color"
+            }
+        , blue600 =
+            { value = "#0A69CF"
+            , type_ = "color"
+            }
+        , blue700 =
+            { value = "#0650A0"
+            , type_ = "color"
+            }
+        , blue800 =
+            { value = "#043F81"
+            , type_ = "color"
+            }
+        , blue900 =
+            { value = "#073165"
+            , type_ = "color"
+            }
+        , blue1000 =
+            { value = "#081E3F"
+            , type_ = "color"
+            }
+        , purple100 =
+            { value = "#F4ECF6"
+            , type_ = "color"
+            }
+        , purple200 =
+            { value = "#EADAEE"
+            , type_ = "color"
+            }
+        , purple300 =
+            { value = "#DCBDE4"
+            , type_ = "color"
+            }
+        , purple400 =
+            { value = "#CC9FD9"
+            , type_ = "color"
+            }
+        , purple500 =
+            { value = "#AA61C2"
+            , type_ = "color"
+            }
+        , purple600 =
+            { value = "#914DA9"
+            , type_ = "color"
+            }
+        , purple700 =
+            { value = "#733B85"
+            , type_ = "color"
+            }
+        , purple800 =
+            { value = "#592D68"
+            , type_ = "color"
+            }
+        , purple900 =
+            { value = "#462352"
+            , type_ = "color"
+            }
+        , purple1000 =
+            { value = "#32183A"
+            , type_ = "color"
+            }
+        , pink100 =
+            { value = "#F9EBF0"
+            , type_ = "color"
+            }
+        , pink200 =
+            { value = "#F6D7E0"
+            , type_ = "color"
+            }
+        , pink300 =
+            { value = "#F5C1D1"
+            , type_ = "color"
+            }
+        , pink400 =
+            { value = "#F190B4"
+            , type_ = "color"
+            }
+        , pink500 =
+            { value = "#EB4F8E"
+            , type_ = "color"
+            }
+        , pink600 =
+            { value = "#B9336B"
+            , type_ = "color"
+            }
+        , pink700 =
+            { value = "#932653"
+            , type_ = "color"
+            }
+        , pink800 =
+            { value = "#711D41"
+            , type_ = "color"
+            }
+        , pink900 =
+            { value = "#591734"
+            , type_ = "color"
+            }
+        , pink1000 =
+            { value = "#401026"
+            , type_ = "color"
+            }
+        }
+    }
+
+
+
+-- DIMENSION
+
+
+dimension =
+    { scale =
+        { dimension0 =
+            { value = "0px"
+            , type_ = "dimension"
+            }
+        , dimension1 =
+            { value = "1px"
+            , type_ = "dimension"
+            }
+        , dimension2 =
+            { value = "2px"
+            , type_ = "dimension"
+            }
+        , dimension3 =
+            { value = "4px"
+            , type_ = "dimension"
+            }
+        , dimension4 =
+            { value = "8px"
+            , type_ = "dimension"
+            }
+        , dimension5 =
+            { value = "12px"
+            , type_ = "dimension"
+            }
+        , dimension6 =
+            { value = "16px"
+            , type_ = "dimension"
+            }
+        , dimension7 =
+            { value = "20px"
+            , type_ = "dimension"
+            }
+        , dimension8 =
+            { value = "24px"
+            , type_ = "dimension"
+            }
+        , dimension9 =
+            { value = "28px"
+            , type_ = "dimension"
+            }
+        , dimension10 =
+            { value = "32px"
+            , type_ = "dimension"
+            }
+        , dimension11 =
+            { value = "36px"
+            , type_ = "dimension"
+            }
+        , dimension12 =
+            { value = "40px"
+            , type_ = "dimension"
+            }
+        , dimension13 =
+            { value = "48px"
+            , type_ = "dimension"
+            }
+        , dimension14 =
+            { value = "56px"
+            , type_ = "dimension"
+            }
+        , dimension15 =
+            { value = "64px"
+            , type_ = "dimension"
+            }
+        , dimension16 =
+            { value = "72px"
+            , type_ = "dimension"
+            }
+        , dimension17 =
+            { value = "80px"
+            , type_ = "dimension"
+            }
+        , dimension18 =
+            { value = "96px"
+            , type_ = "dimension"
+            }
+        }
+    , breakpoint =
+        { small =
+            { value = "640px"
+            , type_ = "dimension"
+            }
+        , medium =
+            { value = "768px"
+            , type_ = "dimension"
+            }
+        , large =
+            { value = "1024px"
+            , type_ = "dimension"
+            }
+        , extraLarge =
+            { value = "1280px"
+            , type_ = "dimension"
+            }
+        }
+    }
+
+
+
+-- ELEVATION
+
+
+elevation =
+    { opacity =
+        { scale =
+            { opacity0 =
+                { value = 0
+                , type_ = "number"
+                }
+            , opacity1 =
+                { value = 0.1
+                , type_ = "number"
+                }
+            , opacity2 =
+                { value = 0.2
+                , type_ = "number"
+                }
+            , opacity3 =
+                { value = 0.3
+                , type_ = "number"
+                }
+            , opacity4 =
+                { value = 0.4
+                , type_ = "number"
+                }
+            , opacity5 =
+                { value = 0.5
+                , type_ = "number"
+                }
+            , opacity6 =
+                { value = 0.6
+                , type_ = "number"
+                }
+            , opacity7 =
+                { value = 0.7
+                , type_ = "number"
+                }
+            , opacity8 =
+                { value = 0.8
+                , type_ = "number"
+                }
+            , opacity9 =
+                { value = 0.9
+                , type_ = "number"
+                }
+            , opacity10 =
+                { value = 1.0
+                , type_ = "number"
+                }
+            }
+        }
+    }
+
+
+
+-- TYPOGRAPHY
+
+
+typography =
+    { fontFamily =
+        { primary =
+            { value = "Roboto"
+            , type_ = "fontFamily"
+            , description = "Serendieは和文Noto Sans JP、欧文Robotoを採用しています。Figmaは和欧混植に対応していないため、Robotoのみ指定することで、和文はFigmaのフォントフォールバックによりNoto Sans JPが自動適用されることを意図しています。これにより実質的な混植を行います。CSSでは一般的なfont-family指定により混植します。"
+            }
+        , monospace =
+            { value = "Noto Sans Mono"
+            , type_ = "fontFamily"
+            }
+        }
+    , fontWeight =
+        { regular =
+            { value = 400
+            , type_ = "fontWeight"
+            }
+        , bold =
+            { value = 700
+            , type_ = "fontWeight"
+            }
+        }
+    , lineHeight =
+        { none =
+            { value = 1
+            , type_ = "number"
+            }
+        , tight =
+            { value = 1.4
+            , type_ = "number"
+            }
+        , normal =
+            { value = 1.6
+            , type_ = "number"
+            }
+        , relaxed =
+            { value = 1.8
+            , type_ = "number"
+            }
+        }
+    , scale =
+        { expanded =
+            { fourExtraSmall =
+                { value = "10px"
+                , type_ = "dimension"
+                }
+            , threeExtraSmall =
+                { value = "11px"
+                , type_ = "dimension"
+                }
+            , twoExtraSmall =
+                { value = "12px"
+                , type_ = "dimension"
+                }
+            , extraSmall =
+                { value = "13px"
+                , type_ = "dimension"
+                }
+            , small =
+                { value = "14px"
+                , type_ = "dimension"
+                }
+            , medium =
+                { value = "16px"
+                , type_ = "dimension"
+                }
+            , large =
+                { value = "18px"
+                , type_ = "dimension"
+                }
+            , extraLarge =
+                { value = "21px"
+                , type_ = "dimension"
+                }
+            , twoExtraLarge =
+                { value = "26px"
+                , type_ = "dimension"
+                }
+            , threeExtraLarge =
+                { value = "32px"
+                , type_ = "dimension"
+                }
+            , fourExtraLarge =
+                { value = "43px"
+                , type_ = "dimension"
+                }
+            , fiveExtraLarge =
+                { value = "64px"
+                , type_ = "dimension"
+                }
+            }
+        , compact =
+            { twoExtraSmall =
+                { value = "10px"
+                , type_ = "dimension"
+                }
+            , extraSmall =
+                { value = "11px"
+                , type_ = "dimension"
+                }
+            , small =
+                { value = "12px"
+                , type_ = "dimension"
+                }
+            , medium =
+                { value = "14px"
+                , type_ = "dimension"
+                }
+            , large =
+                { value = "16px"
+                , type_ = "dimension"
+                }
+            , extraLarge =
+                { value = "19px"
+                , type_ = "dimension"
+                }
+            , twoExtraLarge =
+                { value = "22px"
+                , type_ = "dimension"
+                }
+            , threeExtraLarge =
+                { value = "28px"
+                , type_ = "dimension"
+                }
+            , fourExtraLarge =
+                { value = "37px"
+                , type_ = "dimension"
+                }
+            , fiveExtraLarge =
+                { value = "56px"
+                , type_ = "dimension"
+                }
+            }
+        }
+    }
